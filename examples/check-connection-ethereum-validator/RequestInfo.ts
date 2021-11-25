7c2277f1f51286df82749b0086288478da76a6a1
stepniowskip <piotr.stepniowski@fujitsu.com>
2021-11-25 14:49:45 +0100

    test: blp with connectivity check
    Business Logic Plugin that checks connectivity between ethereum
    validator and ledger
    
    Signed-off-by: stepniowskip <piotr.stepniowski@fujitsu.com>

---
+++ examples/check-connection-ethereum-validator/RequestInfo.ts
@@ -0,0 +1,12 @@
+export class RequestInfo {
+  businessLogicID: string;
+  tradeID: string;
+
+  setBusinessLogicID(businessLogicID: string) {
+    this.businessLogicID = businessLogicID;
+  }
+
+  setTradeID(tradeID: string) {
+    this.tradeID = tradeID;
+  }
+}
